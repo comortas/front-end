8823a574de31b279d6a2f1fb8dfcb884ababde77
kishoreag <kishoreag@cloudix.io>
2022-09-20 12:44:42 +0530

    userinfo pulled in header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem } from 'reactstrap';
 import logo from './../../assets/images/logo.png';
+import _get from 'lodash/get';
 
 const Header = () => {
+	const { userInfo } = useSelector(({ sessionReducer }) => {
+		return {
+			userInfo: _get(sessionReducer, 'userInfo', false)
+		};
+	});
+	console.log('userInfo: ', userInfo);
+
 	const [ isOpen, setIsOpen ] = useState(false);
 
 	const toggle = () => setIsOpen(!isOpen);
