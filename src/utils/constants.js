31ff1205ce0a107a9b2cbd80c7be3164ec32c106
kishoreag <kishoreag@cloudix.io>
2022-09-24 15:38:18 +0530

    map key updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const clientId = '15322983434-5kkbn9bnlsrj0ggk44ckjuc7o060srub.apps.googleusercontent.com';
-export const mapsApiKey = 'AIzaSyCpU3ZQd1SkaJ0kVboXB7QcFSnZDkVTPQM';
+export const mapsApiKey = 'AIzaSyA3Jgy11egWFYQu7ty3SyloYDJ_VGGq0mQ';
